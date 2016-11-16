5c767a94721834432ef8307ac5f2722a7e1335be
Stacy <stacytrent@gmail.com>
2016-11-16 14:26:37 -0500

    started updatingbut didnt have time to really dig in

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# new_sequel_burger
+# burger
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Big Mac');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken');
+INSERT INTO burgers(burger_name) VALUES ('A1 Thick N Hearty');
+INSERT INTO burgers(burger_name) VALUES ('The Whopper');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
